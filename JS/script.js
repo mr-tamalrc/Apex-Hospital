ddf0273e0eb3f5cbe1aa798bc6bfc912d299fa7d
Tamal Roy Chowdhury <roytamal556@gmail.com>
2026-03-08 14:48:54 +0530

    separate JS file

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+const admissionForm = document.getElementById("admission-form");
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+admissionForm.addEventListener("submit", function () {
+	const slots = {
+		"Dr. Amit Sharma": ["10:00 AM", "11:00 AM", "12:00 PM"],
+		"Dr. Arthur Concepion": ["01:00 PM", "02:00 PM", "03:00 PM"],
+		"Dr. Rahul Roy": ["04:00 PM", "05:00 PM"],
+		"Dr. Herman Garter": ["10:00 AM", "02:00 PM"],
+		"Dr. Arjun Ghosh": ["12:00 PM", "03:00 PM", "06:00 PM"],
+	};
+
+	function loadSlots() {
+		const doctor = document.getElementById("doctor").value;
+		const timeSelect = document.getElementById("time");
+		timeSelect.innerHTML = "";
+		slots[doctor].forEach((t) => {
+			let opt = document.createElement("option");
+			opt.text = t;
+			timeSelect.add(opt);
+		});
+	}
+});
+
+const userDetails = document.getElementsByClassName("user-details");
