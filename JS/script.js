7140db4ecb80c10402bcbb1344c27b42dc99c8d4
Tamal Roy Chowdhury <roytamal556@gmail.com>
2026-03-09 06:48:05 +0530

    name change in application

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,29 @@
-const admissionForm = document.getElementById("admission-form");
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const phone = document.getElementById("phone").value;
-admissionForm.addEventListener("submit", function () {
-	const slots = {
-		"Dr. Amit Sharma": ["10:00 AM", "11:00 AM", "12:00 PM"],
-		"Dr. Arthur Concepion": ["01:00 PM", "02:00 PM", "03:00 PM"],
-		"Dr. Rahul Roy": ["04:00 PM", "05:00 PM"],
-		"Dr. Herman Garter": ["10:00 AM", "02:00 PM"],
-		"Dr. Arjun Ghosh": ["12:00 PM", "03:00 PM", "06:00 PM"],
-	};
+const slots = {
+	"Dr. Amit Sharma": ["10:00 AM", "11:00 AM", "12:00 PM"],
+	"Dr. Arthur Concepion": ["01:00 PM", "02:00 PM", "03:00 PM"],
+	"Dr. Rahul Roy": ["04:00 PM", "05:00 PM"],
+	"Dr. Herman Garter": ["10:00 AM", "02:00 PM"],
+	"Dr. Arjun Ghosh": ["12:00 PM", "03:00 PM", "06:00 PM"],
+};
 
-	function loadSlots() {
+function loadSlots() {
+	const doctor = document.getElementById("doctor").value;
+	const timeSelect = document.getElementById("time");
+	timeSelect.innerHTML = "";
+	slots[doctor].forEach((t) => {
+		let opt = document.createElement("option");
+		opt.text = t;
+		timeSelect.add(opt);
+	});
+}
+const print = document
+	.getElementById("admission-form")
+	.addEventListener("submit", function (e) {
+		e.preventDefault();
+		const name = document.getElementById("name").value;
+		const email = document.getElementById("email").value;
+		const phone = document.getElementById("phone").value;
+		const time = document.getElementById("time");
 		const doctor = document.getElementById("doctor").value;
-		const timeSelect = document.getElementById("time");
-		timeSelect.innerHTML = "";
-		slots[doctor].forEach((t) => {
-			let opt = document.createElement("option");
-			opt.text = t;
-			timeSelect.add(opt);
-		});
-	}
-});
-
-const userDetails = document.getElementsByClassName("user-details");
+	});
+console.log(print);
